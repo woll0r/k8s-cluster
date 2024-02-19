40aa47b02402f18ce00e788aae55171877cf6700
Wolfgang <woll0r@gmail.com>
2024-02-19 22:58:56 +0000

    feat(task): refactor Taskfiles a bit

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,7 @@
 set -e
 set -o noglob
 
+echo "Startup" > .vscode/postCreateCommand.log
 # Setup fisher plugin manager for fish and install plugins
 /usr/bin/fish -c "
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
@@ -11,9 +12,13 @@ fisher install jorgebucaran/autopair.fish
 fisher install nickeb96/puffer-fish
 fisher install PatrickF1/fzf.fish
 "
+echo "Fisher done" >> .vscode/postCreateCommand.log
 
 # Create/update virtual environment
 if ! grep -q "venv /workspaces/" .venv/pyvenv.cfg; then
     rm -rf .venv
+    echo "Venv cleanup done" >> .vscode/postCreateCommand.log
 fi
+
 task deps
+echo "Task done" >> .vscode/postCreateCommand.log
